e9ca319a4bbaa861738ba69c4c1e9fec1f375da7
Ansik <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2023-12-03 05:38:20 +0000

    modified

--- todo-manager/index.js
+++ todo-manager/index.js
@@ -1,22 +1,27 @@
 const express = require("express")
 const app = express()
 
-app.get("/todos", (request,response) => {
-    response.send("hello World");
-    console.log("Todo List");
+app.post("/",(request,response) => {
+    response.send("Hello World");
+    console.log("WELCOME");
 });
 
-app.post("/todos", (request,response) => {
-    console.log("Creating a todo",request.body);
-});
+// app.get("/todos", (request,response) => {
+//     response.send("hello World");
+//     console.log("Todo List");
+// });
 
-app.post("/todos/:id/markAsCompleted" , (request,response) => {
-    console.log("We have to update a todo with ID" , request.params.id);
-});
+// app.post("/todos", (request,response) => {
+//     console.log("Creating a todo",request.body);
+// });
 
-app.delete("/todos/:id" , (request,response) => {
-    console.log("Delete a todo by ID:", request.params.id);
-});
+// app.post("/todos/:id/markAsCompleted" , (request,response) => {
+//     console.log("We have to update a todo with ID" , request.params.id);
+// });
+
+// app.delete("/todos/:id" , (request,response) => {
+//     console.log("Delete a todo by ID:", request.params.id);
+// });
 
 app.listen(3000, () => {
     console.log("The server has started at port 3000!");
