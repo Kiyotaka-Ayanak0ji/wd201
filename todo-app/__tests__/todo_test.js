4e8002e37b267b4062f3540d21eda3ab37d7fc69
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2023-12-05 22:24:51 +0530

    Commit

--- todo-app/__tests__/todo_test.js
+++ todo-app/__tests__/todo_test.js
@@ -1,11 +1,47 @@
-const db = require("../models/index.js");
+const request = require("supertest");
 
-describe("Tests" , function () {
-    beforeAll(async () => {
-        await db.sequelize.sync({ force: true })
+const db = require("../models/index");
+const app = require("../app");
+
+let server,agent;
+
+describe("Todo test suite",() => {
+    beforeAll(async () =>{
+        await db.sequelize.sync({force: true});
+        server = app.listen(3000,() => { });
+        agent = request.agent(server);
     });
-    test("To check if the page is responsive", async () => {
-        const t = true;
-        expect(t).toBe(true);
+    afterAll(async () => {
+        await db.sequelize.close();
+        server.close();
     });
-})
+    test("responds with json at /todos",async () => {
+        const response  = await agent.post('/todos').send({
+            'title': 'Buy Milk',
+            'dueDate': new Date().toISOString(),
+            'completed': false
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.header["content-type"]).toBe(
+            "application/json; charset=utf-8"
+        );
+        const parsedResponse = JSON.parse(response.text);
+        expect(parsedResponse.id).toBeDefined();
+    });
+    test("Mark a todo as complete",async () => {
+        const response  = await agent.post('/todos').send({
+            'title': 'Buy Milk',
+            'dueDate': new Date().toISOString(),
+            'completed': false
+        });
+        const parsedResponse = JSON.parse(response.text);
+        const todoID = parsedResponse.id;
+
+        expect(parsedResponse.completed).toBe(false);
+        const markCompletedResponse = await agent.put(`/todos/${todoID}/markAsCompleted`).send();
+        const parsedUpdateResponse = JSON.parse(markCompletedResponse.text);
+
+        expect(parsedUpdateResponse.completed).toBe(true);
+    });
+});
+
