73b59f6251b001f61a40c4e6e61defaee255840d
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2023-12-16 16:16:58 +0530

    commit

--- todo-app/app.js
+++ todo-app/app.js
@@ -2,11 +2,32 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path");
 app.use(bodyParser.json());
 
+//Set view Engine as EJS
+app.set("view engine","ejs");
+
+app.get("/", async (request,response) => {
+  const allTodos = await Todo.getTodos();
+  if(request.accepts("html")){
+    response.render('index', {
+      allTodos
+    });
+  }
+  else{
+    response.json({
+      allTodos
+    });
+  }
+
+});
+
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get("/todos", async (request, response) => {
-  const todosItems = await Todo.gettodo();
-  response.send(todosItems);
+  const todos = await Todo.getTodos();
+  return response.json(todos);
 });
 
 app.post("/todos", async (request, response) => {
