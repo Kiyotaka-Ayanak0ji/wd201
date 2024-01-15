f7a171da919e62d48289801e397d57e1e3c0f9d1
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-01-15 09:11:19 +0530

    fixed

--- todo-app/models/user.js
+++ todo-app/models/user.js
@@ -1,5 +1,7 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,17 +15,15 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
   }
-  User.init(
-    {
-      firstName: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "User",
-    },
-  );
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, 
+  {
+    sequelize,
+    modelName: 'User',
+  });
   return User;
 };
