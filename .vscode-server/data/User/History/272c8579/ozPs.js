639b87bb6395a65797b7d94d6836c66a61000572
Kiyotaka-Ayanak0ji <101034731+Kiyotaka-Ayanak0ji@users.noreply.github.com>
2024-01-21 16:37:38 +0530

    final

--- .vscode-server/data/User/History/272c8579/ozPs.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const fs  = require("fs");
-
-fs.readFile("home.html", (err, home) => {
-    console.log(home.toString());
-});
-
-//Listening by the server
-server.listen(3000);
--- wd201_capstone/README.md
+++
@@ -1,2 +0,0 @@
-# wd201_capstone
-A LMS(Learning Management System) for students and teachers.
